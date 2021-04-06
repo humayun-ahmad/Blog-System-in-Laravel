537dbe5b56032bbe49a65b7b3349912aecf490cb
humayun-ahmad <humayunraj789@gmail.com>
2021-04-06 18:07:57 +0600

    bolg site

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        DB::table('users')->insert([
+        	'role_id' => '1',
+        	'name' => 'Admin',
+        	'username' => 'admin',
+        	'email' => 'admin@gamil.com',
+        	'password' => bcrypt('rootadmin'),
+        ]);
+
+        DB::table('users')->insert([
+        	'role_id' => '2',
+        	'name' => 'Author',
+        	'username' => 'author',
+        	'email' => 'author@gamil.com',
+        	'password' => bcrypt('rootauthor'),
+        ]);
+    }
+}
