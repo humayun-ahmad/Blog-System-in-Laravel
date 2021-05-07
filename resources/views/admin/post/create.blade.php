ef47df1a01d02ab26a815e1dc0db45cbc9b7eeb9
humayun-ahmad <humayunraj789@gmail.com>
2021-05-07 14:45:12 +0600

    body-edittor add in add post

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -100,20 +100,20 @@
 
                     </div>
                     <div class="body">
-                        <form action="{{ route('admin.category.store') }}" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            <div class="form-group form-float">
-                                <div class="form-line">
-                                    <input type="text" id="name" class="form-control" name="name">
-                                    <label class="form-label">Category Name</label>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <input type="file" name="image" id="image">
-                            </div>
-                            <a type="button" class="btn btn-danger m-t-15 waves-effect" href="{{ route('admin.category.index') }}">BACK</a>
-                            <button type="submit" class="btn btn-primary m-t-15 waves-effect">SUBMIT</button>
-                        </form>
+                       <textarea id="tinymce">
+                                <h2>WYSIWYG Editor</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ullamcorper sapien non nisl facilisis bibendum in quis tellus. Duis in urna bibendum turpis pretium fringilla. Aenean neque velit, porta eget mattis ac, imperdiet quis nisi. Donec non dui et tortor vulputate luctus. Praesent consequat rhoncus velit, ut molestie arcu venenatis sodales.</p>
+                                <h3>Lacinia</h3>
+                                <ul>
+                                    <li>Suspendisse tincidunt urna ut velit ullamcorper fermentum.</li>
+                                    <li>Nullam mattis sodales lacus, in gravida sem auctor at.</li>
+                                    <li>Praesent non lacinia mi.</li>
+                                    <li>Mauris a ante neque.</li>
+                                    <li>Aenean ut magna lobortis nunc feugiat sagittis.</li>
+                                </ul>
+                                <h3>Pellentesque adipiscing</h3>
+                                <p>Maecenas quis ante ante. Nunc adipiscing rhoncus rutrum. Pellentesque adipiscing urna mi, ut tempus lacus ultrices ac. Pellentesque sodales, libero et mollis interdum, dui odio vestibulum dolor, eu pellentesque nisl nibh quis nunc. Sed porttitor leo adipiscing venenatis vehicula. Aenean quis viverra enim. Praesent porttitor ut ipsum id ornare.</p>
+                            </textarea>
                     </div>
                 </div>
             </div>
@@ -126,6 +126,13 @@
 @endsection
 
 @push('js')
+
+    <!-- Ckeditor -->
+    <script src="{{ asset('assets/backend/plugins/ckeditor/ckeditor.js')  }}"></script>
+    <!-- TinyMCE -->
+    <script src="{{ asset('assets/backend/plugins/tinymce/tinymce.js')  }}"></script>
+    <script src="{{ asset('assets/backend/js/pages/forms/editors.js')  }}"></script>
+
 
     <!-- Select Plugin Js -->
 {{--    <script src="{{asset('assets/backend/plugins/bootstrap-select/js/bootstrap-select.js')}}"></script>--}}
