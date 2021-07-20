764b9ea22d73c10b2c6d135a3acd316b3ec7bdcc
humayun-ahmad <humayunraj789@gmail.com>
2021-07-21 01:48:35 +0600

    topbar manu added

--- resources/views/layouts/backend/partial/topbar.blade.php
+++ resources/views/layouts/backend/partial/topbar.blade.php
@@ -8,10 +8,16 @@
             <div class="collapse navbar-collapse" id="navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Call Search -->
-                    <li><a href="javascript:void(0);" class="js-search" data-close="true"><i class="material-icons">search</i></a></li>
+                    <li>
+                        <a href="javascript:void(0);" class="js-search" data-close="true"><i class="material-icons">search</i></a>
+                    </li>
+                    <li>
+                        <a href="{{ route('home') }}">Home</a>
+                    </li>
+                    
                     <!-- #END# Call Search -->
                     <!-- Notifications -->
-                 
+
                 </ul>
             </div>
         </div>
